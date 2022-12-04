9ed1c5f91730eb997566fe8c335f5cd8351d6040
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2022-12-04 15:04:49 +0100

    first commit

---
+++ Smart Home/Program.cs
@@ -0,0 +1,23 @@
+﻿using Smart_Home.forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smart_Home
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}
