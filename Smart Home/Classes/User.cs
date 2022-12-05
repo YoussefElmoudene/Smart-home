e4531a867a739c7161a89cfda40fe220ec68e35b
youssefelyou <youssef.elyourizi@edu.uca.ma>
2022-12-05 20:39:40 +0100

    Classes added

---
+++ Smart Home/Classes/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Home.Classes
+{
+    internal class User
+    {
+        private int id;
+        private string fullname;
+        private string telephone;
+        private string username;
+        private string password;
+        private int age;
+        private string role;
+
+        public User(int id, string fullname, string telephone, string username, string password, int age, string role)
+        {
+            this.Id = id;
+            this.Fullname = fullname;
+            this.Telephone = telephone;
+            this.Username = username;
+            this.Password = password;
+            this.Age = age;
+            this.Role = role;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Fullname { get => fullname; set => fullname = value; }
+        public string Telephone { get => telephone; set => telephone = value; }
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public int Age { get => age; set => age = value; }
+        public string Role { get => role; set => role = value; }
+    }
+}
