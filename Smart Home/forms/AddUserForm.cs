12fa3fadebef6261139deda9e703bbbae2235ec1
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2022-12-13 18:56:05 +0100

    userForm

--- Smart Home/forms/AddUserForm.cs
+++ Smart Home/forms/AddUserForm.cs
@@ -1,4 +1,6 @@
-﻿using Smart_Home.Classes;
+﻿using MySqlConnector;
+using Smart_Home.Classes;
+using Smart_Home.Connextion;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Smart_Home.forms
 {
@@ -35,6 +38,22 @@ namespace Smart_Home.forms
         {
             User user = new User(0,fullname.Text,telephone.Text,username.Text,password.Text, Convert.ToInt32(age.Text),role.SelectedItem.ToString());
             MessageBox.Show(user.ToString());
+            MyConnextion connextion = new MyConnextion();
+            connextion.open();
+            MySqlCommand cmd = connextion.getConnextion().CreateCommand();
+
+            cmd.CommandText = "INSERT INTO `user`(`id`, `fullname`, `telephone`, `username`, `password`, `age`, `role`) VALUES" +
+                "VALUES(@id, @fullname,@telephone,@username,@password,@age,@role)";
+            cmd.Parameters.AddWithValue("@id", null);
+            cmd.Parameters.AddWithValue("@fullname", user.Fullname);
+            cmd.Parameters.AddWithValue("@telephone", user.Telephone);
+            cmd.Parameters.AddWithValue("@username", user.Username);
+            cmd.Parameters.AddWithValue("@password", user.Password);
+            cmd.Parameters.AddWithValue("@age", user.Age);
+            cmd.Parameters.AddWithValue("@role", user.Role);
+
+
+            cmd.ExecuteNonQuery();
         }
     }
 }
